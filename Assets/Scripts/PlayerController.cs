b1e079df2b37a6196cff05c550d2309f710cf3a1
lichen014 <121971822+lichen014@users.noreply.github.com>
2023-03-07 14:29:19 -0800

    issue with collision and powerups

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,7 +8,8 @@ public class PlayerController : MonoBehaviour
     private GameObject focalPoint;
     private float powerUpStrength = 15.0f;
     public float speed = 5.0f;
-    public bool hasPowerup;
+    public bool hasPowerup = false;
+    public GameObject powerupIndicator;
 
     // Start is called before the first frame update
     void Start()
@@ -21,7 +22,10 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         float forwardInput = Input.GetAxis("Vertical");
+        
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -29,6 +33,7 @@ public class PlayerController : MonoBehaviour
         if(other.CompareTag("Powerup"))
         {
             hasPowerup = true;
+            powerupIndicator.gameObject.SetActive(true);
             Destroy(other.gameObject);
             StartCoroutine(PowerUpCountdownRoutine());
         }
@@ -38,6 +43,7 @@ public class PlayerController : MonoBehaviour
     {
         yield return new WaitForSeconds(7);
         hasPowerup = false;
+        powerupIndicator.gameObject.SetActive(false);
     }
 
     private void OnCollision(Collision collision)
